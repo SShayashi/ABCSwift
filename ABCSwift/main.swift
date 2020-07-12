31c2f1919466e5c9ff8d3ddf21df76192ea2ab15
hayashi <sshayashi0208@gmail.com>
2020-07-12 17:30:02 +0900

    Initial commit

--- ABCSwift/main.swift
+++ ABCSwift/main.swift
@@ -1,12 +1,53 @@
-//
-//  main.swift
-//  ABCSwift
-//
-//  Created by sshayashi on 2020/07/12.
-//  Copyright © 2020 sshayashi. All rights reserved.
-//
-
 import Foundation
 
-print("Hello, World!")
+extension String {
+    func toIntList() -> [Int] {
+        return self.split(separator: " ").map{Int($0)!}
+    }
+    
+    func toList() -> [String] {
+        return self.map{String($0)}
+    }
+    
+    func count(c:Character) -> Int{
+        var result = 0
+
+        for e in self{
+            if e == c{
+                result += 1
+            }
+        }
+        return result
+    }
+}
+
+extension Int {
+    func toBin() -> String {
+        return String(self, radix: 2)
+    }
+    
+    func toD() -> Double {
+        return Double(self)
+    }
+}
+
+extension Double {
+    func toI() -> Int {
+        return Int(self)
+    }
+}
+
+extension Array where Element: Equatable{
+    func count(n:Self.Element) -> Int{
+        var result = 0
+        for e in self{
+            if e == n{
+                result += 1
+            }
+        }
+        return result
+    }
+}
+
+
 
