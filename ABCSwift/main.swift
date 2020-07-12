d7aadd76d2256700dc2f8a3ddfb41645046251aa
hayashi <sshayashi0208@gmail.com>
2020-07-12 09:19:50 +0900

    Initial Commit

---
+++ ABCSwift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ABCSwift
+//
+//  Created by sshayashi on 2020/07/12.
+//  Copyright © 2020 sshayashi. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
