f75e10902e73ef3443e1dca9e9f0e0a0e5dd34db
hayashi <sshayashi0208@gmail.com>
2020-07-16 00:33:15 +0900

    WA

---
+++ ABCSwift/abc/disco2020/disco2020c.swift
@@ -0,0 +1,75 @@
+
+func disco2020c() -> [[String]]{
+    let HWK = readLine()!.toIntList()
+    let H = HWK[0]
+    let W = HWK[1]
+    var K = HWK[2]
+    var G:[[String]] = []
+    var ANS:[[Int]] = Array(repeating: Array(repeating: 0, count: W), count: H)
+    for y in 1...H{
+        let x = readLine()!.toList()
+        G.append(x)
+    }
+    var isInStbY = false
+    var width:[Int] = []
+    var yLists:[[Int]] = []
+    var list:[Int] = []
+
+
+    for y in 0..<H{
+        if G[y].contains("#"){
+            list.append(y)
+            yLists.append(list)
+            list = []
+        }else{
+            list.append(y)
+        }
+    }
+    if list.count > 0 {
+        yLists.append(list)
+    }
+    
+    func isInStrovery(_ yList:[Int],_ x:Int) -> Bool{
+        for y in yList{
+            if G[y][x].contains("#"){
+                return true
+            }
+        }
+        return false
+    }
+    for (index,yList) in yLists.enumerated(){
+        var xLists:[[Int]] = []
+        var xlist:[Int] = []
+        var isInStbX = false
+        for x in 0..<W{
+            if isInStrovery(yList, x){
+                xlist.append(x)
+                xLists.append(xlist)
+                xlist = []
+            }else{
+                xlist.append(x)
+            }
+        }
+        if xlist.count > 0 {xLists.append(xlist)}
+        
+        
+        for xlist in xLists{
+            for x in xlist{
+                for y in yList{
+                    ANS[y][x] = K
+                }
+            }
+            K -= 1
+        }
+    }
+    
+    for y in 0..<H{
+        for x in 0..<W{
+            print(ANS[y][x],terminator: "")
+        }
+        print("")
+    }
+    
+    
+    return G
+}
