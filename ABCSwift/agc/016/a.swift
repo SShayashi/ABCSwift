1ad4a74ef6c1b7e83472e635e7cb196b3422fe7f
hayashi <sshayashi0208@gmail.com>
2020-07-19 15:33:06 +0900

    😆AC

--- ABCSwift/agc/016/a.swift
+++ ABCSwift/agc/016/a.swift
@@ -1,11 +1,26 @@
-import Foundation
-
-func agc016a(){
-    let S = readLine()!
+func agc016a() -> Int{
+    var S = readLine()!
     let N = S.count
-    for i in 0..<N{
-        for j in 0..<N{
-//            全探索？
+    let counter = S.counter()
+    var answer = 999
+    for c in counter.keys{
+        var t = S
+        var a = 0
+        while t.count(c: c) != t.count {
+            var tmp = ""
+            for i in 0..<t.count-1 {
+                let index = t.index(t.startIndex, offsetBy: i)
+                let indexPlus = t.index(t.startIndex, offsetBy: i+1)
+                if t[index] == c || t[indexPlus] == c{
+                    tmp.append(c)
+                } else {
+                    tmp.append(t[index])
+                }
+            }
+            a += 1
+            t = tmp
         }
+        answer = min(a, answer)
     }
+    return answer
 }
