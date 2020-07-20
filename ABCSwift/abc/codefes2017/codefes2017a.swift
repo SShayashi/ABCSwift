6a554a62283b14fd384120fb0a1ab533715e5e17
hayashi <sshayashi0208@gmail.com>
2020-07-20 23:14:59 +0900

    😊AC

---
+++ ABCSwift/abc/codefes2017/codefes2017a.swift
@@ -0,0 +1,17 @@
+func codefes2017a() -> String {
+    let NMK = readLine()!.toIntList()
+    let N = NMK[0]
+    let M = NMK[1]
+    let K = NMK[2]
+    var tmp = 0
+    for n in 0...N{
+        for m in 0...M{
+            var x = n*(M-m)
+            var y = m*(N-n)
+            if (x+y) == K{
+                return "Yes"
+            }
+        }
+    }
+    return "No"
+}
