2fbdad4d59ba744de2a77c65db0d3528eb9dea66
hayashi <sshayashi0208@gmail.com>
2020-08-02 00:09:02 +0900

    解説AC

--- ABCSwift/abc/133/abc133d.swift
+++ ABCSwift/abc/133/abc133d.swift
@@ -1,6 +1,16 @@
 func abc133c(){
     let N = Int(readLine()!)!
     let A = readLine()!.toSepIntList()
-    var X = Array(repeating: 0, count: N+1)
+    var X = Array(repeating: 0, count: N)
+    let total = A.reduce(0, +)
+    var sum = 0
+    for i in stride(from: 1, to: N, by: 2){
+        sum += A[i]
+    }
+    X[0] = total - 2*sum
     
+    for i in 1..<N {
+        X[i] = 2*A[i-1] - X[i-1]
+    }
+    X.dump()
 }
