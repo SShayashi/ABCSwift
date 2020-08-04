243e91ec19b6472c77424a76f82570a7d8856ab1
hayashi <sshayashi0208@gmail.com>
2020-08-04 23:46:31 +0900

    解説AC

---
+++ ABCSwift/abc/174/abc174e.swift
@@ -0,0 +1,19 @@
+
+func e() -> Int {
+    let NK = readLine()!.toSepIntList()
+    let N = NK[0]
+    let K = NK[1]
+    var A = readLine()!.toSepIntList()
+    var left = 0, right = 1000000001
+    while right-left > 1 {
+        let mid = (left + right) / 2
+        func f(_ x: Int) -> Bool {
+            var now = 0
+            for a in A { now += (a - 1) / x }
+            return now <= K
+        }
+        if f(mid) { right = mid }
+        else { left = mid  }
+    }
+    return right
+}
